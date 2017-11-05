e37086bfb57e548c98886b4746315d0ce11600df
fabriciomorber <fabmorber@gmail.com>
2017-11-05 18:25:44 -0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public frase:string;
+  public frases = new Array();
+  private indice:number;
+
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  novaFrase() {
+    this.indice = Math.random()*(this.frases.length);
+    this.frases[0]="Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos";
+    this.frases[1]="O sucesso normalmente vem para quem está ocupado demais para procurar por ele";
+    this.frases[2]="Se você não está disposto a arriscar, esteja disposto a uma vida comum";
+    this.frase = this.frases[parseInt(this.indice)];
   }
 
 }
